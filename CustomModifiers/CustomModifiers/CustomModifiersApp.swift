5f513f511b94161ef86609e9007c478ba3b9b6ec
Dean Thompson <deanthompson.music@gmail.com>
2022-06-12 14:02:11 +0900

    Initial commit

---
+++ CustomModifiers/CustomModifiers/CustomModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifiersApp.swift
+//  CustomModifiers
+//
+//  Created by Dean Thompson on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct CustomModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
