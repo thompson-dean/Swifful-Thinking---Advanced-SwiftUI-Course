7020a99d3f06f3b884e0c252f074936258f24461
Dean Thompson <deanthompson.music@gmail.com>
2022-07-14 22:11:36 +0900

    Finished lesson 5 - need to add notes

--- 05_CustomShapesBootcamp/CustomShapesBootcamp/CustomShapesBootcamp.swift
+++ 05_CustomShapesBootcamp/CustomShapesBootcamp/CustomShapesBootcamp.swift
@@ -7,9 +7,68 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+            
+        }
+    }
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
+
+struct Rhombus: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
+
 struct CustomShapesBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Trapezoid()
+                
+//                .fill(LinearGradient(colors: [.purple, .yellow], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round))
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 300)
+                
+        }
     }
 }
 
