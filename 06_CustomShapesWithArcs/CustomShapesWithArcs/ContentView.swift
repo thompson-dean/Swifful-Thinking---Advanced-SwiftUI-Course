3f9f032045a2091929f8c4988a33466d0e621dbb
Dean Thompson <deanthompson.music@gmail.com>
2022-07-27 14:22:45 +0900

    Learnt how to use addArc. Still a little bit left to do in this lesson

--- 06_CustomShapesWithArcs/CustomShapesWithArcs/ContentView.swift
+++ 06_CustomShapesWithArcs/CustomShapesWithArcs/ContentView.swift
@@ -7,19 +7,45 @@
 
 import SwiftUI
 
-struct Trangle: Shape {
+struct ArcSample: Shape {
     func path(in rect: CGRect) -> Path {
-        
-       
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 270),
+                endAngle: Angle(degrees: 360),
+                clockwise: false)
+        }
     }
-    
-    
+}
+
+struct ShapeWithArc: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            
+//            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY ),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false)
+            
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        }
+        
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ShapeWithArc()
+            .frame(width: 300, height: 300)
     }
 }
 
