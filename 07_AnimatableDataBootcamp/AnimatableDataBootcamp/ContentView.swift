738fb56b5ea5ada91e75caa10b3b5c7d40590220
Dean Thompson <deanthompson.music@gmail.com>
2022-08-02 13:54:52 +0900

    Completed Lesson 7

--- 07_AnimatableDataBootcamp/AnimatableDataBootcamp/ContentView.swift
+++ 07_AnimatableDataBootcamp/AnimatableDataBootcamp/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animate: Bool = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            PacMan(offsetAmount: animate ? 20 : 0.0001)
+//            RectangleWithSingleCornerAnimation(cornerRadius: animate ? 0 : 30)
+                .frame(width: 250, height: 250)
+                
+        }
+        .onAppear {
+            withAnimation(.easeInOut.repeatForever()) {
+                animate.toggle()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RectangleWithSingleCornerAnimation: Shape {
+    
+    var animatableData: Double {
+        get { cornerRadius }
+        set { cornerRadius = newValue }
+    }
+    
+    var cornerRadius: Double
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            
+            path.addArc(
+                center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
+                radius: cornerRadius,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 90),
+                clockwise: false)
+            
+            path.addLine(to: CGPoint(x: rect.maxX  - cornerRadius, y: rect.maxY))
+
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
+
+struct PacMan: Shape {
+    
+    var animatableData: CGFloat {
+        get { offsetAmount }
+        set { offsetAmount = newValue }
+    }
+    
+    var offsetAmount: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: -offsetAmount),
+                endAngle: Angle(degrees: offsetAmount),
+                clockwise: true)
+        }
     }
 }
--- 07_AnimatableDataBootcamp/README.md
+++ 07_AnimatableDataBootcamp/README.md
@@ -1,5 +1,49 @@
 ## Lesson 7 - Animate Custom shapes with AnimateableData in SwiftUI
 
-```
+In this lesson we learnt how to animate custom shapes!
+It is quite simple! The main difference between animating custom shapes and shapes of Apple's APIs is that you need to use AnimatableData. AnimatableData is needed as SwiftUI does not know the value of explicit paths in custom shapes. It has to be fed these values through AnimatableData with get {} set {}.
 
+Below is an animationg of PacMan eating.
+
+```
+struct ContentView: View {
+
+    @State private var animate: Bool = true
+
+    var body: some View {
+
+        ZStack {
+            PacMan(offsetAmount: animate ? 20 : 0)
+                .frame(width: 250, height: 250)
+
+        }
+        .onAppear {
+            withAnimation(.easeInOut.repeatForever()) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct PacMan: Shape {
+
+    var animatableData: Double {
+        get { offsetAmount }
+        set { offsetAmount = newValue }
+    }
+
+    var offsetAmount: Double
+
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: -offsetAmount),
+                endAngle: Angle(degrees: offsetAmount),
+                clockwise: true)
+        }
+    }
+}
 ```
