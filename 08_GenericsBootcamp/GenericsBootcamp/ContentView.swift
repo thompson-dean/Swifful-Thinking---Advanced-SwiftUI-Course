60baac3efa2871495a7a04926bfed25369dc9f72
Dean Thompson <deanthompson.music@gmail.com>
2022-08-03 12:56:06 +0900

    Half way through Lesson 8

--- 08_GenericsBootcamp/GenericsBootcamp/ContentView.swift
+++ 08_GenericsBootcamp/GenericsBootcamp/ContentView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+struct StringModel {
+    
+    let info: String?
+    
+    func removeInfo() -> StringModel {
+        StringModel(info: nil)
+    }
+}
+
+struct BoolModel {
+    
+    let info: Bool?
+    
+    func removeInfo() -> BoolModel {
+        BoolModel(info: nil)
+    }
+}
+
+class ViewModel: ObservableObject {
+    
+    @Published var stringModel = StringModel(info: "Hello, World!")
+    @Published var boolModel = BoolModel(info: true)
+    
+    func removeData() {
+        stringModel = stringModel.removeInfo()
+        boolModel = boolModel.removeInfo()
+    }
+    
+}
+
 struct ContentView: View {
+    
+    @StateObject private var vm = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text(vm.stringModel.info ?? "No Data")
+            Text(vm.boolModel.info?.description ?? "No data")
+        }
+        .onTapGesture {
+            vm.removeData()
+        }
     }
 }
 
